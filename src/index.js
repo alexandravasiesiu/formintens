f10413b46a8ab86210a06ec11cd95919a803c9dd
Alexandra <alexandra@gypaetuscode.com>
2023-09-30 13:35:15 +0300

    :boom: hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const container = document.getElementById("root");
@@ -8,8 +8,8 @@ const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter basename='/'>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
